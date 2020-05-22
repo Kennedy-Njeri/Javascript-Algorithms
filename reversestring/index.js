60792c42425d68b5f7719827cff42f746ccaeb07
Kennedy Njeri <mistakenz123@gmail.com>
2020-05-22 19:50:36 +0300

    reverse function

--- reversestring/index.js
+++ reversestring/index.js
@@ -1,30 +1,16 @@
-// --- Directions
-// Given a string, return a new string with the reversed
-// order of characters
-// --- Examples
-//   reverse('apple') === 'leppa'
-//   reverse('hello') === 'olleh'
-//   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-  return str.split('').reduce((rev, char) => char + rev, '');
+
+
+const reverse = (str) => {
+  // solution 1
+  return str.split('').reverse().join('')
+  // const arr = str.split('')
+  // arr.reverse()
+  // return arr.join('')
 }
 
-module.exports = reverse;
+console.log(reverse('kennedy'))
 
-// function reverse(str) {
-//   return str
-//     .split('')
-//     .reverse()
-//     .join('');
-// }
 
-// function reverse(str) {
-//   let reversed = '';
-//
-//   for (let character of str) {
-//     reversed = character + reversed;
-//   }
-//
-//   return reversed;
-// }
+
+module.exports = reverse
