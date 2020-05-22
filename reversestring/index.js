0420e88488e02d2c55f6a0302998678e20f74f3e
Kennedy Njeri <mistakenz123@gmail.com>
2020-05-22 20:35:14 +0300

    reverse function solution 2

--- reversestring/index.js
+++ reversestring/index.js
@@ -1,16 +1,39 @@
 
 
 
-const reverse = (str) => {
-  // solution 1
-  return str.split('').reverse().join('')
-  // const arr = str.split('')
-  // arr.reverse()
-  // return arr.join('')
-}
+
 
 console.log(reverse('kennedy'))
 
 
 
 module.exports = reverse
+
+
+// const reverse = (str) => {
+//   // solution 1
+//   return str.split('').reverse().join('')
+//   // const arr = str.split('')
+//   // arr.reverse()
+//   // return arr.join('')
+// }
+
+// solution 2
+// const reverse = (str) => {
+//
+//   let reversed = ''
+//
+//   for (let word of str) {
+//     //console.log(word)
+//     console.log(word + reversed)
+//     reversed = word + reversed
+//   }
+//
+//   return reversed
+//   // const arr = []
+//   //
+//   // for (let word of str.split('').reverse()) {
+//   //   arr.push(word)
+//   // }
+//   // return arr.join('')
+// }
