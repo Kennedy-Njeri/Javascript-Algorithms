c658b474282652def766a76508c88ae86aa9f17d
Kennedy Njeri <mistakenz123@gmail.com>
2020-05-28 19:15:44 +0300

    array chunk solution

---
+++ array chunking/index.js
@@ -0,0 +1,37 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+// finding last element in an array
+//var array = [1,2,3,4,5,6];
+//var val = array[array.length - 1]; ans=6
+
+
+
+const chunk = (array, size) => {
+    const chunked = []
+
+    for (let element of array) {
+        const last = chunked[chunked.length -1]
+
+        if (!last || last.length === size) {
+            chunked.push([element])
+        } else {
+            last.push(element)
+        }
+    }
+
+    return chunked
+}
+
+
+console.log(chunk([1, 2, 5, 5, 7], 2))
+
+module.exports = chunk;
+
