04e9cac6c39ec664ca6f34e6b93e03fa9f7d5291
Kennedy Njeri <mistakenz123@gmail.com>
2020-06-09 18:14:30 +0300

    recursive examples

---
+++ steps/recursion.js
@@ -0,0 +1,28 @@
+
+
+const recursive = (n) => {
+    if (n === 0) {
+        // return no more work
+        return
+    }
+
+
+    console.log(n)
+    recursive(n -1)
+}
+
+
+
+recursive(5)
+
+
+
+
+// imperative approach
+function countDownFrom(number) {
+    for (let i = number; i > 0; i--) {
+        console.log(i);
+    }
+}
+
+countDownFrom(5);
