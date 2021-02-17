d907bdb2adb03ea891e0772756185e274277ab87
Kennedy Njeri <mistakenz123@gmail.com>
2021-02-17 22:19:09 +0300

    big o calculations2

---
+++ big o/sol2.js
@@ -0,0 +1,22 @@
+const challageTwo = (input) => {
+    let a = 5 // o(1)
+    let b = 7 // 0(1)
+    let c = 10 // o(1)
+
+    for (let i = 0; i < input.length; i++) {
+        let x = i + 1 // o(n)
+        let y = i + 2 // o(n)
+        let z = i + 3 // o(n)
+
+    }
+
+    for (let j = 0; i < input.length; j++) {
+        let p = j + 1 // o(n)
+        let q= j + 2 // o(n)
+    }
+
+    let  word = "i'm known" // o(1)
+
+}
+
+challageTwo([5, 8, 9, 10])
