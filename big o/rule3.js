8b7cfdfad9c9ddb49b6534a04099944ea0a97b47
Kennedy Njeri <mistakenz123@gmail.com>
2021-02-18 17:45:57 +0300

    big o calculations3

---
+++ big o/rule3.js
@@ -0,0 +1,15 @@
+// different terms for inputs
+
+const  printBoxesTwice = (boxes, boxes2) => {
+    boxes.forEach((box) => {
+        console.log(box)
+    })
+
+
+    boxes2.forEach((box2) => {
+        console.log(box2)
+    })
+}
+
+
+// in this case  o(a+ b) if they are nested o(a*b)
