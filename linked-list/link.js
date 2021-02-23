94af1b0f1d10145091cc3aac2a8e79d52acdfbd3
Kennedy Njeri <mistakenz123@gmail.com>
2021-02-23 18:01:33 +0300

    insert linked list

--- linked-list/link.js
+++ linked-list/link.js
@@ -1,5 +1,14 @@
 
 
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+
+
 
 class LinkedList {
     constructor(value) {
@@ -13,24 +22,52 @@ class LinkedList {
     }
 
     append(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
+        const newNode = new Node(value)
         this.tail.next = newNode
         this.tail = newNode
         this.length++
         return this
     }
     prepend(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
+        const newNode = new Node(value)
         newNode.next = this.head
         this.head = newNode
         this.length++
         return this
+    }
+    printList() {
+        const array = []
+        let currentNode = this.head
+        while (currentNode !== null) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array
+    }
+    insert(index, value) {
+        // check params
+        if (index >= this.length){
+            return this.append(value)
+        }
+
+        const newNode = new Node(value)
+
+        const leader = this.traverseToIndex(index-1)
+        const holdingPointer = leader.next
+        leader.next = newNode
+        newNode.next = holdingPointer
+        this.length ++
+        return this.printList()
+    }
+
+    traverseToIndex(index) {
+        let counter = 0
+        let currentNode = this.head
+        while (counter !==index) {
+            currentNode = currentNode.next
+            counter++
+        }
+        return currentNode
     }
 }
 
@@ -40,5 +77,7 @@ const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(3)
+myLinkedList.insert(2, 55)
 
-console.log(myLinkedList)
+
+console.log(myLinkedList.printList())
