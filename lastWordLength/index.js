c5ebe6936c4f37c40deea921136a505c6d3404d7
Kennedy-Njeri <mistakenz123@gmail.com>
2021-05-30 17:26:28 +0300

    length of last word in a string

---
+++ lastWordLength/index.js
@@ -0,0 +1,19 @@
+const lengthOfLastWord = (x) => {
+    let newInput = x.trim();
+    let sArr = newInput.split("");
+    let counter = 0;
+
+    if (newInput.length === 0) return counter;
+
+    if (!sArr.includes(" ")) return newInput.length;
+
+    for (let i = newInput.length - 1; i > 0; i--) {
+        if (newInput[i] != " ") {
+            counter++;
+        } else {
+            return counter;
+        }
+    }
+}
+
+console.log(lengthOfLastWord("30 seconds of codes"))
